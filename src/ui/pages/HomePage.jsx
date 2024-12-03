c462c7907c85bfe1e8fc52ddbf4f35b1646c96e3
MarinSH <marine.sml@hotmail.com>
2024-12-03 17:33:06 +0100

    🎉 init project

--- src/ui/pages/HomePage.jsx
+++ src/ui/pages/HomePage.jsx
@@ -6,9 +6,7 @@ export default function HomePage() {
     <section className="bg-gray-50">
       <div className="mx-auto max-w-screen-xl px-4 py-32 flex h-screen items-center">
         <div className="mx-auto max-w-xl text-center">
-          <h1 className="text-3xl font-extrabold sm:text-5xl">
-            Template
-            <strong className="font-extrabold text-secondary-700 sm:block">Electron + React</strong>
+          <h1 className="text-3xl font-extrabold sm:text-5xl"> trakap
           </h1>
           <p className="mt-4 sm:text-xl/relaxed">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
